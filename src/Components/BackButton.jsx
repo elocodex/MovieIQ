c81235249da4795a50ffc621fb54579fff04f88d
Elo <ielowinds@gmail.com>
2025-04-16 15:51:22 +0100

    Edited Styles

--- src/Components/BackButton.jsx
+++ src/Components/BackButton.jsx
@@ -9,14 +9,15 @@ const BackButton = () => {
   };
 
   return (
-    <div className='py-5 fixed top-28'>
-        <button 
-      onClick={goBack}
-      className="flex items-center cursor-pointer ml-10 text-white px-4 py-2 transition-all duration-300 font-medium mb-4"
-    >
-      <i className="fa-solid fa-chevron-left mr-1"></i> Return    
-    </button>
-    </div>
+    <div className="py-4 px-5 fixed z-50 top-24 left-2 sm:top-28 sm:left-3 md:top-32 md:left-6  w-fit">
+      <button 
+        onClick={goBack}
+        className="flex items-center gap-2 bg-dark-100/90 backdrop-blur-md shadow-lg shadow-light-100/10 rounded-2xl text-light-200 px-4 py-2 transition-all duration-300 text-sm font-medium cursor-pointer "
+      >
+        <i className="fa-solid fa-chevron-left text-[12px]"></i>
+        Back
+      </button>
+  </div>
   );
 };
 
